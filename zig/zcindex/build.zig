58ef884af8a1db4d770367a327b9e0af71d691ea
ousttrue <ousttrue@gmail.com>
2026-05-25 22:46:05 +0900

    windows

--- zig/zcindex/build.zig
+++ zig/zcindex/build.zig
@@ -7,14 +7,19 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const llvm_dir = b.option([]const u8, "llvm", "llvm path. like /usr/lib/llvm/20") orelse "/usr";
-    const llvm_include = b.fmt("{s}/include", .{llvm_dir});
-    const llvm_lib = b.fmt("{s}/lib64", .{llvm_dir});
+    var llvm_include: []const u8 = b.fmt("{s}/include", .{llvm_dir});
+    var llvm_lib: []const u8 = b.fmt("{s}/lib64", .{llvm_dir});
+    var llvm_lib_name: []const u8 = "clang";
+    if (target.result.os.tag == .windows) {
+        llvm_include = "C:/Program Files/LLVM/include";
+        llvm_lib = "C:/Program Files/LLVM/lib";
+        llvm_lib_name = "libclang";
+    }
 
     const t = b.addTranslateC(.{
         .target = target,
         .optimize = optimize,
         .root_source_file = .{ .cwd_relative = b.fmt("{s}/clang-c/Index.h", .{llvm_include}) },
-        .use_clang = true,
     });
     t.addIncludePath(.{ .cwd_relative = llvm_include });
 
@@ -31,7 +36,7 @@ pub fn build(b: *std.Build) void {
         .link_libc = true,
         .link_libcpp = true,
     });
-    mod.linkSystemLibrary("clang", .{});
+    mod.linkSystemLibrary(llvm_lib_name, .{});
     mod.addLibraryPath(.{ .cwd_relative = llvm_lib });
 
     const exe = b.addExecutable(.{
